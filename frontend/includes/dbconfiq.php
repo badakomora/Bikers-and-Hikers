20d98919099a0f78df9ca124f7293fb7e618225a
badaa <badaandrew@outlook.com>
2023-02-22 09:10:35 +0300

    third commit

---
+++ frontend/includes/dbconfiq.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "bh_db");
+?>
--- frontend/index.php
+++ frontend/index.php
@@ -1,46 +1,54 @@
 <?php
 session_start();
-include '../includes/dbconfiq.php';
-if (isset($_POST['Login'])) {
+include './includes/dbconfiq.php';
+if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $msg = '';
   
-    $query = mysqli_query($con, "SELECT * FROM users where email = '$email' AND password = '$password'");
+    $query = mysqli_query($con, "SELECT * FROM users where email = '$email'");
     $row = mysqli_fetch_assoc($query);
     if ($row >= 1) {
-      $_SESSION['email'] = $row['email'];
+      if (password_verify( $password, $row['password'])) {
+        
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['email'] = $row['email'];
   
-      header('refresh: 0, ./?p=Dashboard');
-      $msg = "Admin Login Access Granted. First clear with your dashboard before you proceed to anything else. WELCOME!";
-      echo "<script type='text/javascript'>alert('$msg');</script>";
-    
+        header('refresh: 0, ./templates/home.php');
+        $msg = "Login Access Granted. WELCOME!";
+        echo "<script type='text/javascript'>alert('$msg');</script>";
+
+      } else {
+        header('refresh: 0, ./');
+        $msg = "Login Access Denied. Please use the correct credentials";
+        echo "<script type='text/javascript'>alert('$msg');</script>";
+      }
     } else {
   
-      header('refresh: 0, ./login.php');
-      $msg = "Admin Login Access Denied. Please use the correct credentials";
+      header('refresh: 0, ./');
+      $msg = "This user does not exist. Please use the correct credentials";
       echo "<script type='text/javascript'>alert('$msg');</script>";
-      
     }
-  
-}elseif (isset($_POST['Register'])) {
     
-  $email = $_POST['email'];
-  $password = $_POST['password'];
+  }elseif (isset($_POST['Register'])){
+   
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-  $query = mysqli_query($con, "SELECT * FROM admin where email = '$email' AND password = '$password'");
-  $row = mysqli_fetch_assoc($query);
-  if ($row >= 1) {
-    $_SESSION['email'] = $row['email'];
 
-    header('refresh: 0, ./?p=Dashboard');
-    $msg = "Admin Login Access Granted. First clear with your dashboard before you proceed to anything else. WELCOME!";
-    echo "<script type='text/javascript'>alert('$msg');</script>";
-  } else {
+    $users = mysqli_query($con, "SELECT * FROM users WHERE email='$email'");
+    $rows = mysqli_num_rows($users);
+    if($rows >= 1){
+        $msg = "This user ALready Exists. Please use a different email to sign Up.";
+        echo "<script type='text/javascript'>alert('$msg');</script>";
+        header("refresh: 0, ./"); 
+    }else{
+        $insert = mysqli_query($con, "INSERT INTO users(email, password) VALUES('$email', '$password')");
+        $msg = "Registration Successful! Procced to sign in.";
+        echo "<script type='text/javascript'>alert('$msg');</script>";
+        header("refresh: 0, ./#SignIn"); 
+    }
 
-    header('refresh: 0, ./login.php');
-    $msg = "Admin Login Access Denied. Please use the correct credentials";
-    echo "<script type='text/javascript'>alert('$msg');</script>";
-  }
 }
 ?>
 
@@ -59,18 +67,18 @@ if (isset($_POST['Login'])) {
     <div class="vh-100 d-flex justify-content-center align-items-center" id="SignIn">
         <div class="col-md-5 p-5 shadow-sm border rounded-5 border-secondary bg-white">
             <h2 class="text-center mb-4 text-secondary">Sign In Form</h2>
-            <form>
+            <form action="" method="POST">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control border border-secondary" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email" name="email" class="form-control border border-secondary" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control border border-secondary" id="exampleInputPassword1">
+                    <input type="password" name="password" class="form-control border border-secondary" id="exampleInputPassword1">
                 </div>
                 <!-- <p class="small"><a class="text-secondary" href="forget-password.html">Forgot password?</a></p> -->
                 <div class="d-grid">
-                    <button class="btn btn-secondary" type="submit">Sign In</button>
+                    <button class="btn btn-secondary" name="login" type="submit">Sign In</button>
                 </div>
             </form>
             <div class="mt-3">
@@ -84,18 +92,18 @@ if (isset($_POST['Login'])) {
     <div class="vh-100 d-flex justify-content-center align-items-center" id="SignUp">
         <div class="col-md-5 p-5 shadow-sm border rounded-5 border-secondary bg-white">
             <h2 class="text-center mb-4 text-secondary">Sign Up Form</h2>
-            <form>
+            <form method="Post" action="">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control border border-secondary" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email" name="email" class="form-control border border-secondary" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control border border-secondary" id="exampleInputPassword1">
+                    <input type="password" name="password" class="form-control border border-secondary" id="exampleInputPassword1">
                 </div>
                 <p class="small"><a class="text-secondary" href="forget-password.html">Forgot password?</a></p>
                 <div class="d-grid">
-                    <button class="btn btn-secondary" id="In" type="submit">Sign Up</button>
+                    <button class="btn btn-secondary" name="Register" type="submit">Sign Up</button>
                 </div>
             </form>
             <div class="mt-3">
