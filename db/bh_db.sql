194010308d815560a84b64be4daa9dec1dc5e6a5
badaa <badaandrew@outlook.com>
2023-02-24 09:53:57 +0300

    db update

--- db/bh_db.sql
+++ db/bh_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2023 at 12:07 PM
+-- Generation Time: Feb 24, 2023 at 07:52 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`) VALUES
+(1, 'Superadmin', 'superadmin@gmail.com', '$2y$10$ZZ7Tbux3KiwsZPG6qP01NuR5v5dYp2J3PLQsMpJs2TvgzOtXxegM2');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comments`
 --
 
@@ -40,7 +60,8 @@ CREATE TABLE `comments` (
 
 INSERT INTO `comments` (`id`, `post_id`, `user_id`, `message`) VALUES
 (8, 5, 3, 'bullshit'),
-(9, 5, 3, 'okay');
+(9, 5, 3, 'okay'),
+(10, 9, 5, 'helllooooooooooooooooooooooooooooo');
 
 -- --------------------------------------------------------
 
@@ -64,7 +85,9 @@ CREATE TABLE `posts` (
 
 INSERT INTO `posts` (`id`, `user_id`, `title`, `message`, `timer`, `file`, `file_ext`) VALUES
 (5, 3, 'Lorem ipsum dolor sit amet, consectetur adip.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.', '2023-02-22 14:39:40', '_MG_2348.jpg', 'jpg'),
-(6, 3, 'Lorem ipsum dolor sit amet, consectetur adip.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.', '2023-02-22 16:46:23', '_MG_2378.jpg', 'jpg');
+(6, 3, 'Lorem ipsum dolor sit amet, consectetur adip.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.', '2023-02-22 16:46:23', '_MG_2378.jpg', 'jpg'),
+(7, 5, 'Lorem ipsum dolor sit amet, consectetur adip.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.', '2023-02-24 05:33:55', '_MG_2334.jpg', 'jpg'),
+(9, 5, 'Lorem ipsum dolor sit amet, consectetur adLorem ipsum dolor sit amet, consectetur ad', 'Lorem ipsum dolor sit amet, consectetur adLorem ipsum dolor sit amet, consectetur adLorem ipsum dolor sit amet, consectetur ad', '2023-02-24 06:32:56', '63e4f969589379.95832296.webp', 'webp');
 
 -- --------------------------------------------------------
 
@@ -76,8 +99,8 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `profile` varchar(100) NOT NULL DEFAULT 'avator.png'
+  `password` varchar(1000) NOT NULL,
+  `profile` varchar(100) NOT NULL DEFAULT 'avator.jpg'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -85,11 +108,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `profile`) VALUES
-(3, 'Masika', 'shillamasika@gmail.com', '$2y$10$p.0DoLZF8AbUEleyY./s.ukoiqUyViHrp3EgtGJmG.TGwcii7Reci', 'avator.jpg');
+(3, 'Masika', 'shillamasika@gmail.com', '$2y$10$p.0DoLZF8AbUEleyY./s.ukoiqUyViHrp3EgtGJmG.TGwcii7Reci', 'avator.jpg'),
+(5, 'Bada', 'badakomora06@gmail.com', '$2y$10$FHOUJ1RG9V9nElE8KBjGqO.nGVTp761Aw1TwmVWzJVjdAq14xmgcW', 'avator.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `comments`
@@ -114,22 +144,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
