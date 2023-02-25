6de7cab137a36237a24c05f5009ebe64ed00c417
badaa <badaandrew@outlook.com>
2023-02-25 11:25:12 +0300

    final system update

--- backend/templates/header.php
+++ backend/templates/header.php
@@ -1,6 +1,11 @@
 <?php 
 session_start();
 include '../../includes/dbconfiq.php';
+if(!isset($_SESSION['email'])) {
+$msg = "Please Sign In Correctly or your Account will be De-activated Completely!";
+echo "<script type='text/javascript'>alert('$msg');</script>";
+header("refresh: 0, ../");
+}
 ?>
 
 <!DOCTYPE html>
